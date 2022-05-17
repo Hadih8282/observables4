a6fb63028a0e476ebf14d36d1dafd4862330cc3f
Hadih8282 <hadi_h82@gmail.com>
2022-05-17 19:17:26 +0430

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, map, take, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'observables4';
+
+  ngOnInit(): void {
+    
+    fromEvent(document,'click').pipe(
+      map(event => event as MouseEvent),
+      map(item => {
+        return {
+          X: item.offsetX,
+          Y: item.offsetY
+        }
+      }),
+      // take(5),
+      // takeWhile(item => item.X < 1000)
+      // tap(i => console.log(i))
+    ).subscribe(console.log);
+
+  }
+
 }
